4e55573ed5f2054a09ec493f13bf93fd7a8dc22d
anuja-jayaweera <anuja.bharatha@gmail.com>
2026-08-25 14:14:57 +0530

    Added the Person class

---
+++ src/main/java/com/example/clinicflow/Classes/Person.java
@@ -0,0 +1,48 @@
+package com.example.clinicflow.Classes;
+
+    public abstract class Person {
+
+        private String id;
+        private String name;
+        private String phone;
+
+        //Constructor of the class
+        public Person(String id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+
+        public abstract String getRole();
+
+        @Override
+        public String toString() {
+            return getRole() + "{id='" + id + "', name='" + name + "', phone='" + phone + "'}";
+        }
+    }
+
